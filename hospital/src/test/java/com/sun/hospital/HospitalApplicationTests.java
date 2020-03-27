d75cc90a2054ff37ccaf285a03dfeed0841012f4
syt <1501935266@qq.com>
2020-03-28 04:03:53 +0800

    Signed-off-by: syt <1501935266@qq.com>

---
+++ hospital/src/test/java/com/sun/hospital/HospitalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sun.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.websocket.Session;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
